3491813a58cdd34167b7db8861e2446f8cf5d949
Kevin Arne <kevin.arne@gmail.com>
2021-11-17 21:43:35 -0800

    Implemented function to find the neighbors, but it's awfully inelegant

--- supports.py
+++ supports.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
 def neighbors(x, y, width, height):
+    neigh = []
     # Check for boundaries (top, left, right, bottom)
     left = False
     right = False
@@ -15,6 +16,33 @@ def neighbors(x, y, width, height):
     if (y == height - 1):
         bottom = True
 
+    # Top left
+    if (not top and not left):
+        neigh.append((x - 1, y - 1))
+    # Top middle
+    if (not top):
+        neigh.append((x, y - 1))
+    # Top right
+    if (not top and not right):
+        neigh.append((x + 1, y - 1))
+    # Left
+    if (not left):
+        neigh.append((x - 1, y))
+    # Right
+    if (not right):
+        neigh.append((x + 1, y))
+    # Bottom left
+    if (not bottom and not left):
+        neigh.append((x - 1, y + 1))
+    # Bottom middle
+    if (not bottom):
+        neigh.append((x, y + 1))
+    # Bottom right
+    if (not bottom and not right):
+        neigh.append((x + 1, y + 1))
+
+    return neigh
+
 filename = input("What is the filename with extension?\n")
 
 # Import the bitmap
@@ -24,9 +52,10 @@ im_width, im_height = img.size
 
 # Scan through bitmap several times, depending on slope of walls
 for x in range(im_width):
-    for y in range(3):
-        print(x, y, im_width, im_height)
-        neighbors(x, y, im_width, im_height)
+    for y in range(1):
+        print(x, y)
+        n = neighbors(x, y, im_width, im_height)
+        print(n)
     # For each loop
     # Check whether the pixel is white
         # If white, check whether neighbors are the color of the previous step (black for first iteration)
